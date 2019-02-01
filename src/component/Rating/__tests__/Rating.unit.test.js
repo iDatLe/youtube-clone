affb3e37dceced6368ab7124e172946d5bbf8d99
Dat <ledatdac@gmail.com>
2019-01-31 16:55:50 -0800

    name changes

--- src/component/Rating/__tests__/Rating.unit.test.js
+++ src/component/Rating/__tests__/Rating.unit.test.js
@@ -4,7 +4,9 @@ import { Rating } from '../Rating';
 
 describe('Rating', () => {
   test('renders', () => {
-    const wrapper = shallow(<Rating/>);
+    const wrapper = shallow(
+    <Rating />
+    );
     expect(wrapper).toMatchSnapshot();
   });
 });
--- src/containers/Watch/Watch.js
+++ src/containers/Watch/Watch.js
@@ -2,13 +2,13 @@ import React from 'react';
 import './Watch.scss';
 import { Video } from '../../component/Video/Video'; 
 import { RelatedVideos } from '../../component/RelatedVideos/RelatedVideos';
-import { VideoMetaData } from '../../component/VideoMetaData/VideoMetaData';
+import { VideoMetadata } from '../../component/VideoMetaData/VideoMetadata';
 
 export function Watch() {
     return (
         <div className="watch-grid">
             <Video id='WMTx590rIMw' />
-            <VideoMetaData className='metadata' viewCount={1000} />
+            <VideoMetadata className='metadata' viewCount={1000} />
             <div className='video-info-box' style={{width: '100%', height: '100px', background: '#BD10E0'}}>Video Info box</div>
             <div className='comments' style={{width: '100%', height: '100px', background: '#9013FE'}}>comments</div>
             <RelatedVideos className='relatedVideos'/>
