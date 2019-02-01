ca5a70a22bc0c3263ac060b99990933bf953091c
Dat <ledatdac@gmail.com>
2019-01-31 16:15:25 -0800

    Meta data

---
+++ src/component/VideoMetaData/VideoMetaData.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import './VideoMetaData.scss';
+import { Rating } from '../Rating/Rating';
+import { Button, Divider, Icon } from 'semantic-ui-react';
+
+export function VideoMetaData(props) {
+
+    const viewCount = Number(props.viewCount).toLocaleString() || '';
+
+    return (
+        <div className='video-metadata'>
+            <h3>Video title</h3>
+
+            <div className='video-stats'>
+                <span>{viewCount} views</span>
+
+                <div className='video-actions' >
+                    <Rating likeCount={1000} dislikeCount={100} />
+                    <Button basic icon labelPosition='left'>
+                        <Icon name='share' />
+                        Share
+                    </Button>
+                    <Button basic icon>
+                        <Icon name='ellipsis horizontal' />
+                    </Button>
+                </div>
+            </div>
+            <Divider />
+        </div>
+    )
+}
--- src/containers/Watch/Watch.js
+++ src/containers/Watch/Watch.js
@@ -2,12 +2,13 @@ import React from 'react';
 import './Watch.scss';
 import { Video } from '../../component/Video/Video'; 
 import { RelatedVideos } from '../../component/RelatedVideos/RelatedVideos';
+import { VideoMetaData } from '../../component/VideoMetaData/VideoMetaData';
 
 export function Watch() {
     return (
         <div className="watch-grid">
             <Video id='WMTx590rIMw' />
-            <div className='metadata' style={{width: '100%', height: '100px', background: '#F91112'}}>Metadata</div>
+            <VideoMetaData className='metadata' viewCount={1000} />
             <div className='video-info-box' style={{width: '100%', height: '100px', background: '#BD10E0'}}>Video Info box</div>
             <div className='comments' style={{width: '100%', height: '100px', background: '#9013FE'}}>comments</div>
             <RelatedVideos className='relatedVideos'/>
